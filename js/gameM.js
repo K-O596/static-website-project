0ed6dad3af180a8644ca1ba1789d21e769ed20ba
KO <2iroquoispliskin@gmail.com>
2023-12-20 19:57:41 +0900

    fix gameM.js

--- js/gameM.js
+++ js/gameM.js
@@ -1,21 +1,30 @@
 // Monty holl problem
 
+// get all img elements
+var images = document.querySelectorAll('img');
+
+images.forEach(function(img) {
+  img.addEventListener('click', function() {
+      // get custom datas from imgs
+      var customData = this.dataset.box;
+      console.log('This is:', customData);
+  });
+});
+
 function Game() {
   // boxes
   const boxes = 3;
 
   // randam boxes
   const correctBox = Math.floor(Math.random() * boxes) + 1;
-  console.log(correctBox);
-  console.log("choose a 1,2 or 3 box.");
+  console.log(`correctbox is ${correctBox}`);
+  console.log("choose an 1, 2 or 3 box.");
 
   // player choise
-  const playerChoice = parseInt(prompt("choose a 1, 2 or 3 box.:"), 10);
+  const playerChoice = parseInt(prompt("choose an 1, 2 or 3 box.:"), 10);
   console.log(playerChoice);
 
-  if (playerChoice == 0) {
-    console.log("Fisish it!");
-  } else {
+  if (playerChoice >= 1 && playerChoice <= 3) {
     let hostOpenBox;
     do {
       hostOpenBox = Math.floor(Math.random() * boxes) + 1;
@@ -23,13 +32,13 @@ function Game() {
 
     console.log(`The host opened box ${hostOpenBox}.`);
 
-    // a chance of changing the choise
+    // a chance of changing the choice
     const changeChoice = confirm("Do you change the choice");
 
     // finalchoice
     const finalChoice = changeChoice
-      ? 6 - playerChoice - hostOpenBox
-      : playerChoice;
+      ? 6 - playerChoice - hostOpenBox //It'll work if you choose OK, and this answer must be an unchoosed number.
+      : playerChoice; //Else
 
     console.log(`Final choice is ${finalChoice}.`);
 
@@ -39,6 +48,9 @@ function Game() {
     } else {
       console.log(`faild. correct box is ${correctBox}`);
     }
+  } else {
+    console.log("Finish it!");
+    alert("Finish it!");
   }
 }
 
